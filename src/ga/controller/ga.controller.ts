d9d9930e1f0c5bac16ba12939df6463ad870bbb9
Ousmane Sakho <genieouzog@gmail.com>
2021-03-24 18:42:25 +0000

    google apis report v3

--- src/ga/controller/ga.controller.ts
+++ src/ga/controller/ga.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Post } from '@nestjs/common';
 import { GaService } from '../services/ga.service';
 import { GaMetricPeriod } from '../dto/ga-metric-period';
 
@@ -7,6 +7,34 @@ export class GaController {
     constructor(private readonly gaService: GaService) {}
     @Get('pageviews')
     getGAData(@Query() queryParams: GaMetricPeriod) {
-        return this.gaService.getGAData(queryParams);
+        return this.gaService.getGAPageViews(queryParams);
+    }
+
+    @Get('new-users')
+    getNewUsers(@Query() queryParams: GaMetricPeriod) {
+        return this.gaService.getGANewUsers(queryParams);
+    }
+
+    @Get('bounce-rate')
+    getBounceRate(@Query() queryParams: GaMetricPeriod) {
+        return this.gaService.getBounceRate(queryParams);
+    }
+
+    @Get('audience-source')
+    getAudienceSource(@Query() queryParams: GaMetricPeriod) {
+        return this.gaService.getGAAudienceSource(queryParams);
+    }
+
+    @Get('props')
+    getGAProps(@Query() queryParams: GaMetricPeriod) {
+        return this.gaService.getGAProps(queryParams);
+    }
+
+    @Post('props')
+    insertGAProp(@Query() queryParams: GaMetricPeriod) {
+        return this.gaService.insertProp({
+            'websiteUrl': 'https://coq-custom-6038fb5243f3401ecb54e6ea.netlify.app',
+            'name': 'geneog3'
+          });
     }
 }
--- src/ga/services/ga.service.ts
+++ src/ga/services/ga.service.ts
@@ -3,8 +3,8 @@ import { CLIENT_EMAIL } from '../../config/env';
 import { GaMetricPeriod } from '../dto/ga-metric-period';
 const { google } = require('googleapis');
 const SERVICE_ACCOUNT_KEY_FILE = __dirname+'/../../../key.pem';
-    const scopes = 'https://www.googleapis.com/auth/analytics.readonly';
-    const jwt = new google.auth.JWT(CLIENT_EMAIL, SERVICE_ACCOUNT_KEY_FILE, null, scopes);
+    const scopes = ['https://www.googleapis.com/auth/analytics.readonly', 'https://www.googleapis.com/auth/analytics.edit'];
+    const jwt = new google.auth.JWT("analytics@coq-trotteur.iam.gserviceaccount.com", SERVICE_ACCOUNT_KEY_FILE, null, scopes);
     const view_id = '216836193';
     
 @Injectable()
@@ -13,15 +13,139 @@ export class GaService {
     private http: HttpService
   ) {}
 
-  async getGAData(period: GaMetricPeriod): Promise<any> {
-    const response = await jwt.authorize()
+  async getGANewUsers(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
     const { data } = await google.analytics('v3').data.ga.get({
       'auth': jwt,
       'ids': 'ga:' + view_id,
       ...period,
-      'metrics': 'ga:pageviews'
+      'metrics': 'ga:newUsers',
+      'dimensions': 'ga:date'
     })
     const { totalsForAllResults } = data;
-    return totalsForAllResults["ga:pageviews"];
+    return data // totalsForAllResults["ga:pageviews"];
+  }
+
+  async getBounceRate(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').data.ga.get({
+      'auth': jwt,
+      'ids': 'ga:' + view_id,
+      ...period,
+      'metrics': 'ga:bounceRate',
+      'dimensions': 'ga:pagePath'
+    })
+    const { totalsForAllResults } = data;
+    return data // totalsForAllResults["ga:pageviews"];
+  }
+
+  async getGAPageViews(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').data.ga.get({
+      'auth': jwt,
+      'ids': 'ga:' + view_id,
+      ...period,
+      'metrics': 'ga:pageViews',
+      'dimensions': 'ga:pagePath'
+    })
+    const { totalsForAllResults } = data;
+    return data // totalsForAllResults["ga:pageviews"];
+  }
+
+
+  async getGAAudienceSource(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics({version: 'v3'}).data.ga.get({
+      'auth': jwt,
+      'ids': 'ga:' + view_id,
+      ...period,
+      'metrics': 'ga:userAcquisitionSource',
+      'dimensions': 'ga:acquisitionSourceMedium'
+    })
+    const { totalsForAllResults } = data;
+    return data // totalsForAllResults["ga:pageviews"];
+  }
+
+  async getGAData(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').data.ga.get({
+      'auth': jwt,
+      'ids': 'ga:' + view_id,
+      ...period,
+      'metrics': 'ga:sessions',
+      'dimensions': 'ga:sessionDurationBucket'
+    })
+    const { totalsForAllResults } = data;
+    return data // totalsForAllResults["ga:pageviews"];
+  }
+
+  async getGAProps(period: GaMetricPeriod): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').management.webproperties.get({
+      'auth': jwt,
+      'accountId': '192728133',
+      'webPropertyId': 'UA-192728133-1'
+    })
+    return data;
+  }
+
+  async insertProp(resource: any): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').management.webproperties.insert({
+      'auth': jwt,
+      'accountId': '192728133',
+      resource
+    });
+    const view = await this.insertView(data.id, data.accountId, data.websiteUrl);
+    await this.updateProp(data.accountId, data.id, { defaultProfileId: view.id, name: resource.name, websiteUrl: resource.websiteUrl });
+    return data;
+  }
+
+  async updateProp(accountId: string, webPropertyId: string, resource: any): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').management.webproperties.update({
+      'auth': jwt,
+      accountId,
+      webPropertyId,
+      resource,
+    });
+    return data;
+  }
+
+  async insertView(webPropertyId: string, accountId: string, websiteUrl: string): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').management.profiles.insert({
+      'auth': jwt,
+      accountId,
+      webPropertyId,
+      'resource': {
+        'name': 'Toutes les données du site Web',
+        'eCommerceTracking': true,
+        'enhancedECommerceTracking': true,
+        'currency': 'EUR',
+        websiteUrl,
+        'timezone': 'Europe/Paris'
+      }
+    })
+    return data;
+  }
+
+  async updateView(profileId: string, webPropertyId: string, accountId: string, websiteUrl: string): Promise<any> {
+    await jwt.authorize()
+    const { data } = await google.analytics('v3').management.profiles.update({
+      'auth': jwt,
+      accountId,
+      webPropertyId,
+      profileId,
+      'resource': {
+        'name': 'Toutes les données du site Web',
+        'eCommerceTracking': true,
+        'enhancedECommerceTracking': true,
+        'currency': 'EUR',
+        websiteUrl,
+        'timezone': 'Europe/Paris'
+      }
+    })
+    return data;
   }
 }
